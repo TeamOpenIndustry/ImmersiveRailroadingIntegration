29a0dad95a7dd563d75ec2464ef1613a89845152
Christian Mesh <christianmesh1@gmail.com>
2019-09-21 10:53:44 -0400

    Initial port to forge 1.11.2

--- src/main/java/cam72cam/immersiverailroading/thirdparty/CompatLoader.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/CompatLoader.java
@@ -23,27 +23,12 @@ public class CompatLoader {
 	public static void setup() {
 		invokeStatic("opencomputers", "cam72cam.immersiverailroading.thirdparty.opencomputers.Compat", "init");
 		invokeStatic("immersiveengineering", "cam72cam.immersiverailroading.thirdparty.ImmersiveEngineering", "init");
-		invokeStatic("computercraft", "cam72cam.immersiverailroading.thirdparty.ComputerCraft", "init");
 	}
 
 	public static void init() {
-		invokeStatic("igwmod", "cam72cam.immersiverailroading.thirdparty.IGWMod", "init");
 	}
 
     public static boolean openWiki() {
-		if (Loader.isModLoaded("igwmod")) {
-			// This is lousy code...
-			try {
-				Class<?> cls = Class.forName("igwmod.gui.GuiWiki");
-				Object wiki = cls.newInstance();
-				FMLCommonHandler.instance().showGuiScreen(wiki);
-				Method scf = cls.getMethod("setCurrentFile", String.class, Object[].class);
-				scf.invoke(wiki, "immersiverailroading:home", new Object[]{});
-				return true;
-			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
+        return false;
     }
 }
