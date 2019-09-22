39eae57cbbbe214c2926a768be5fa4f77caf319a
Christian Mesh <christianmesh1@gmail.com>
2019-09-22 11:39:48 -0400

    Disable all for forge 1.7.10

--- src/main/java/cam72cam/immersiverailroading/thirdparty/CompatLoader.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/CompatLoader.java
@@ -8,21 +8,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class CompatLoader {
-	public static Object invokeStatic(String modID, String cname, String method, Object ...objects) {
-		if (Loader.isModLoaded(modID)) {
-			try {
-				Class<?> cls = Class.forName(cname);
-				return cls.getMethod(method).invoke(null, objects);
-			} catch (Exception ex) {
-				ImmersiveRailroading.catching(ex);
-			}
-		}
-		return null;
-	}
 	
 	public static void setup() {
-		invokeStatic("opencomputers", "cam72cam.immersiverailroading.thirdparty.opencomputers.Compat", "init");
-		invokeStatic("immersiveengineering", "cam72cam.immersiverailroading.thirdparty.ImmersiveEngineering", "init");
 	}
 
 	public static void init() {
