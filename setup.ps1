59671c184327baa3c1d7d54482e66f70c0efd2f5
Christian Mesh <christianmesh1@gmail.com>
2019-10-15 08:23:15 -0400

    Switch to using source sets

--- setup.ps1
+++ setup.ps1
@@ -8,8 +8,6 @@ if(Test-Path -Path TrackAPI ){
 
 git clone --branch $branch git@github.com:TeamOpenIndustry/TrackAPI.git
 
-Add-Content -Path settings.gradle -Value "include 'TrackAPI'" 
-
 (Get-Content src/main/java/cam72cam/immersiverailroading/Mod.java) -replace 'required-before:modcore', "required-before:modcore; required-after:trackapi@[1.1,)" | Set-Content src/main/java/cam72cam/immersiverailroading/Mod.java
 
 (Get-Content build.gradle) -replace '^dependencies {', "apply from: 'ImmersiveRailroadingIntegration/dependencies.gradle'`r`ndependencies {" | Set-Content build.gradle 
--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ fi
 rm -rf TrackAPI
 git clone --branch $BRANCH git@github.com:TeamOpenIndustry/TrackAPI.git
 
-echo "include 'TrackAPI'" >> settings.gradle
-
 sed -i src/main/java/cam72cam/immersiverailroading/Mod.java -e 's/required-before:modcore/\0; required-after:trackapi@[1.1,)/'
 
 sed -i build.gradle -e "s,^dependencies {,apply from: 'ImmersiveRailroadingIntegration/dependencies.gradle'\n\0,"
