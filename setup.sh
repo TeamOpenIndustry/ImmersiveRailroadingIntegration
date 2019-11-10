e8e2d66356dc39e0d89f69ed2b6dbeb53fb19f03
Christian Mesh <christianmesh1@gmail.com>
2019-11-10 15:29:51 -0500

    Update setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ fi
 rm -rf TrackAPI
 git clone --branch $BRANCH git@github.com:TeamOpenIndustry/TrackAPI.git
 
-sed -i src/main/java/cam72cam/immersiverailroading/Mod.java -e 's/required-before:universalmodcore/\0; required-after:trackapi@[1.1,)/'
-
-sed -i build.gradle -e "s,^dependencies {,apply from: 'ImmersiveRailroadingIntegration/dependencies.gradle'\n\0,"
+cat src/main/resources/pack.mcmeta<<EOT
+{
+  "pack": {
+    "description": "IR resources",
+    "pack_format": 4
+  }
+}
+EOT
