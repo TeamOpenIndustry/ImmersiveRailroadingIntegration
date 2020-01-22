ceb51aeea665bbaa87b5c4a4e7ca52a6ea6f524d
Christian Mesh <christianmesh1@gmail.com>
2020-01-22 07:52:26 -0500

    Add more control to load phase for integration

--- src/main/java/cam72cam/immersiverailroading/thirdparty/CompatLoader.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/CompatLoader.java
@@ -1,6 +1,7 @@
 package cam72cam.immersiverailroading.thirdparty;
 
 import cam72cam.immersiverailroading.ImmersiveRailroading;
+import cam72cam.mod.ModEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Loader;
 
@@ -8,7 +9,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class CompatLoader {
-	public static Object invokeStatic(String modID, String cname, String method, Object ...objects) {
+	private static Object invokeStatic(String modID, String cname, String method, Object ...objects) {
 		if (Loader.isModLoaded(modID)) {
 			try {
 				Class<?> cls = Class.forName(cname);
@@ -18,16 +19,6 @@ public class CompatLoader {
 			}
 		}
 		return null;
-	}
-	
-	public static void setup() {
-		invokeStatic("opencomputers", "cam72cam.immersiverailroading.thirdparty.opencomputers.Compat", "init");
-		invokeStatic("immersiveengineering", "cam72cam.immersiverailroading.thirdparty.ImmersiveEngineering", "init");
-		invokeStatic("computercraft", "cam72cam.immersiverailroading.thirdparty.ComputerCraft", "init");
-	}
-
-	public static void init() {
-		invokeStatic("igwmod", "cam72cam.immersiverailroading.thirdparty.IGWMod", "init");
 	}
 
     public static boolean openWiki() {
@@ -46,4 +37,25 @@ public class CompatLoader {
 		}
 		return false;
     }
+
+	public static void common(ModEvent event) {
+		switch (event) {
+			case CONSTRUCT:
+				break;
+			case INITIALIZE:
+				break;
+			case SETUP:
+				invokeStatic("immersiveengineering", "cam72cam.immersiverailroading.thirdparty.ImmersiveEngineering", "init");
+				invokeStatic("computercraft", "cam72cam.immersiverailroading.thirdparty.ComputerCraft", "init");
+				invokeStatic("opencomputers", "cam72cam.immersiverailroading.thirdparty.opencomputers.Compat", "init");
+				break;
+			case FINALIZE:
+				invokeStatic("igwmod", "cam72cam.immersiverailroading.thirdparty.IGWMod", "init");
+				break;
+			case START:
+				break;
+			case RELOAD:
+				break;
+		}
+	}
 }
