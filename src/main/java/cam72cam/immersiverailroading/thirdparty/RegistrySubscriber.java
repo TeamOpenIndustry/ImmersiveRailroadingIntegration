d8767637360f69e5d78981b2b5b82446cac67704
Goldenfield192 <1437356849@qq.com>
2026-01-02 21:19:44 +0800

    remap EventBusSubscriber package

--- src/main/java/cam72cam/immersiverailroading/thirdparty/RegistrySubscriber.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/RegistrySubscriber.java
@@ -4,10 +4,10 @@ import cam72cam.immersiverailroading.IRBlocks;
 import cam72cam.immersiverailroading.ImmersiveRailroading;
 import dan200.computercraft.api.peripheral.PeripheralCapability;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
-@Mod.EventBusSubscriber(modid = ImmersiveRailroading.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = ImmersiveRailroading.MODID)
 public class RegistrySubscriber {
     @SubscribeEvent
     public static void onCapabilityRegister(RegisterCapabilitiesEvent event) {
