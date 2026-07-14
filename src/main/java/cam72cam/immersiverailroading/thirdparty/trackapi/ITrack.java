335f16436d357d7d9e5ff8ae05429cf553028119
Philipp <philippdal@gmail.com>
2026-07-14 17:50:37 +0200

    rerefix: Vec3

--- src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/ITrack.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/ITrack.java
@@ -6,6 +6,8 @@ import cam72cam.mod.world.World;
 import trackapi.lib.ITrackV2;
 import trackapi.lib.Util;
 
+import net.minecraft.world.phys.Vec3;
+
 public interface ITrack {
     static boolean isRail(World world, Vec3i pos) {
         return get(world, new Vec3d(pos), true) != null;
@@ -49,7 +51,7 @@ public interface ITrack {
             }
 
             @Override
-            public <D extends trackapi.lib.PathingData> void getNextPosition(D pos, net.minecraft.util.math.Vec3d vel, double gauge) {
+            public <D extends trackapi.lib.PathingData> void getNextPosition(D pos, Vec3 vel, double gauge) {
                 ITrack.this.getNextPosition(IRPathingData.wrap(pos), new Vec3d(vel), gauge);
             }
         };
--- src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/TileEntityTickableTrack.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/TileEntityTickableTrack.java
@@ -3,7 +3,8 @@ package cam72cam.immersiverailroading.thirdparty.trackapi;
 import cam72cam.mod.ModCore;
 import cam72cam.mod.block.tile.TileEntityTickable;
 import cam72cam.mod.resource.Identifier;
-import net.minecraft.util.math.Vec3d;
+
+import net.minecraft.world.phys.Vec3;
 
 public class TileEntityTickableTrack extends TileEntityTickable implements trackapi.lib.ITrackV2 {
     static {
@@ -30,7 +31,7 @@ public class TileEntityTickableTrack extends TileEntityTickable implements track
     }
 
     @Override
-    public <D extends trackapi.lib.PathingData> void getNextPosition(D pos, Vec3d mot, double gauge) {
+    public <D extends trackapi.lib.PathingData> void getNextPosition(D pos, Vec3 mot, double gauge) {
         track().getNextPosition(pos, mot, gauge);
     }
 }
