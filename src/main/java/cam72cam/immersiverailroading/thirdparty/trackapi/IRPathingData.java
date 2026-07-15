439a59e781ccf35d80fab8adae766358377904f4
Philipp <philippdal@gmail.com>
2026-07-15 08:34:03 +0200

    1.16.5 forge Vec3 fix (#26)
    * fix: wrong vector3d
    
    * re-fix: Vec3

--- src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/IRPathingData.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/trackapi/IRPathingData.java
@@ -1,6 +1,7 @@
 package cam72cam.immersiverailroading.thirdparty.trackapi;
 
 import cam72cam.mod.math.Vec3d;
+import net.minecraft.world.phys.Vec3;
 
 public class IRPathingData extends trackapi.lib.PathingData {
     private Vec3d posCache;
@@ -79,7 +80,7 @@ public class IRPathingData extends trackapi.lib.PathingData {
      */
     @Deprecated
     @Override
-    public trackapi.lib.PathingData setPos(net.minecraft.util.math.Vec3d pos) {
+    public trackapi.lib.PathingData setPos(Vec3 pos) {
         super.setPos(pos);
         if (this.posCache == null || !this.posCache.internal().equals(pos)) {
             Vec3d newPos = new Vec3d(pos);
