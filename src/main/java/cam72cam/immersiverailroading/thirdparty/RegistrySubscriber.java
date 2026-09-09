c21022b3a6d2a54a90a77ac67a97460e3435d4ed
Goldenfield192 <1437356849@qq.com>
2026-09-09 13:37:40 +0800

    another uncommited for prev commit

--- src/main/java/cam72cam/immersiverailroading/thirdparty/RegistrySubscriber.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/RegistrySubscriber.java
@@ -1,20 +1,20 @@
-//package cam72cam.immersiverailroading.thirdparty;
-//
-//import cam72cam.immersiverailroading.IRBlocks;
-//import cam72cam.immersiverailroading.ImmersiveRailroading;
-//import dan200.computercraft.api.peripheral.PeripheralCapability;
-//import net.neoforged.bus.api.SubscribeEvent;
-//import net.neoforged.fml.common.EventBusSubscriber;
-//import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
-//import net.neoforged.fml.ModList;
-//
-//@EventBusSubscriber(modid = ImmersiveRailroading.MODID)
-//public class RegistrySubscriber {
-//    @SubscribeEvent
-//    public static void onCapabilityRegister(RegisterCapabilitiesEvent event) {
-//        if (ModList.get().isLoaded("computercraft")) {
-//            event.registerBlock(PeripheralCapability.get(), ComputerCraft.run.get(),
-//                                IRBlocks.BLOCK_RAIL.internal, IRBlocks.BLOCK_RAIL_GAG.internal);
-//        }
-//    }
-//}
+package cam72cam.immersiverailroading.thirdparty;
+
+import cam72cam.immersiverailroading.IRBlocks;
+import cam72cam.immersiverailroading.ImmersiveRailroading;
+import dan200.computercraft.api.peripheral.PeripheralCapability;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.fml.ModList;
+
+@EventBusSubscriber(modid = ImmersiveRailroading.MODID)
+public class RegistrySubscriber {
+    @SubscribeEvent
+    public static void onCapabilityRegister(RegisterCapabilitiesEvent event) {
+        if (ModList.get().isLoaded("computercraft")) {
+            event.registerBlock(PeripheralCapability.get(), ComputerCraft.run.get(),
+                                IRBlocks.BLOCK_RAIL.internal, IRBlocks.BLOCK_RAIL_GAG.internal);
+        }
+    }
+}
